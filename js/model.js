d39c49e4330c2fb03c210a48ad36615b5fd1d1a7
Aaron Cremona <dubbacremona@gmail.com>
2016-04-12 21:59:29 -0700

    added seed data to model

---
+++ js/model.js
@@ -0,0 +1,22 @@
+var model = {
+  todayIds: ['1', '2'].
+  thisWeekIds: ['3', '4'],
+  items: {
+    '1': {
+      text: 'Create clever sample to do\'s',
+      isComplete: false
+    },
+    '2': {
+      text: 'Throw tennis ball for dog',
+      isComplete: false
+    },
+    '3': {
+      text: 'Buy milk',
+      isComplete: false
+    },
+    '4': {
+      text: 'Make plans to takeover the world',
+      isComplete: false
+    }
+  }
+}
