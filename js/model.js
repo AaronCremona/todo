47e4a531643ef7cdc3fdfef30761577d63a31f63
Aaron Cremona <dubbacremona@gmail.com>
2016-04-12 23:11:09 -0700

    refactored data structure to work with .map

--- js/model.js
+++ js/model.js
@@ -1,24 +1,34 @@
 var m = {
-  lists: {
-    today: ['1', '2'],
-    week: ['3', '4']
-  }
-  items: {
-    '1': {
+  lists: [
+    {
+      id: "#today",
+      items: ['1', '2']
+    },
+    {
+      id: "#week",
+      items: ['3', '4']
+    }
+  ],
+  items: [
+    {
+      id: '1',
       text: 'Create clever sample to do\'s',
       isComplete: false
     },
-    '2': {
+    {
+      id: '2',
       text: 'Throw tennis ball for dog',
       isComplete: false
     },
-    '3': {
+    {
+      id: '3',
       text: 'Buy milk',
       isComplete: false
     },
-    '4': {
+    {
+      id: '4',
       text: 'Make plans to takeover the world',
       isComplete: false
     }
-  }
+  ]
 }
